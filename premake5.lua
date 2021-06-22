6998573f13b7d9927c2168370a29968304713c1d
Tony Yu <tony2u@gmail.com>
2021-06-22 20:51:54 +0800

    SDL2_mixer added & MinGW supported

--- premake5.lua
+++ premake5.lua
@@ -1,16 +1,20 @@
 -- premake5.lua
-location "build"
+if (string.sub(_ACTION, 1, 2) == "vs") then
+    location "build"
+else
+    location "build_mingw"
+end
 
 solution "SDL2"
 do
     configurations { "Debug", "Release" }
-    platforms { "Win32", "Win64" }
+    platforms { "Win64", "Win32" }
 end
 
 filter "configurations:Debug"
 do
     defines { "DEBUG", "_DEBUG" }
-    flags { "Symbols" }
+    symbols "On"
 end
 
 filter "configurations:Release"
@@ -19,19 +23,45 @@ do
     optimize "On"
 end
 
-filter { "platforms:Win32" }
-   architecture "x32"
-filter { "platforms:Win64" }
-   architecture "x64"
+filter "platforms:Win32"
+do
+    system "windows"
+    architecture "x86"
+end
 
-filter {"platforms:Win32", "configurations:Debug" }
-    targetdir "build/Win32/Debug"
-filter {"platforms:Win32", "configurations:Release" }
-    targetdir "build/Win32/Release"
-filter {"platforms:Win64", "configurations:Debug" }
-    targetdir "build/Win64/Debug"
-filter {"platforms:Win64", "configurations:Release" }
-    targetdir "build/Win64/Release"
+filter "platforms:Win64"
+do
+    system "windows"
+    architecture "x86_64"
+end
+
+if (_ACTION == "vs2015") then
+    toolset "msc-v140"
+elseif (_ACTION == "vs2017") then
+    toolset "msc-v141"
+elseif (_ACTION == "vs2019") then
+    toolset "msc-v142"
+end
+
+if (string.sub(_ACTION, 1, 2) == "vs") then
+    filter {"platforms:Win32", "configurations:Debug" }
+        targetdir "build/lib/%{cfg.architecture}/Debug"
+    filter {"platforms:Win32", "configurations:Release" }
+        targetdir "build/lib/%{cfg.architecture}/Release"
+    filter {"platforms:Win64", "configurations:Debug" }
+        targetdir "build/lib/%{cfg.architecture}/Debug"
+    filter {"platforms:Win64", "configurations:Release" }
+        targetdir "build/lib/%{cfg.architecture}/Release"
+else
+    filter {"platforms:Win32", "configurations:Debug" }
+        targetdir "build_mingw/lib/%{cfg.architecture}/Debug"
+    filter {"platforms:Win32", "configurations:Release" }
+        targetdir "build_mingw/lib/%{cfg.architecture}/Release"
+    filter {"platforms:Win64", "configurations:Debug" }
+        targetdir "build_mingw/lib/%{cfg.architecture}/Debug"
+    filter {"platforms:Win64", "configurations:Release" }
+        targetdir "build_mingw/lib/%{cfg.architecture}/Release"
+end
 
 dofile "zlib.lua"
 dofile "freetype.lua"
@@ -40,4 +70,4 @@ dofile "SDL2.lua"
 dofile "SDL2_gfx.lua"
 dofile "SDL2_image.lua"
 dofile "SDL2_ttf.lua"
-
+dofile "SDL2_mixer.lua"
